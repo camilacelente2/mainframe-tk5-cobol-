209cfd316b4ae54401ade5811548d8b361c1af09
Camila Celente. <camilacelente@yahoo.com.br>
2025-08-12 20:39:24 -0300

    Create BOOKCLI.cob

---
+++ BOOKCLI.cob
@@ -0,0 +1,4 @@
+ 000001            01  REG-CLIENTES.              
+ 000002                05 REG-CODIGO   PIC 9(05). 
+ 000003                05 REG-NOME     PIC X(20). 
+ 000004                05 REG-ENDERECO PIC X(25). 
