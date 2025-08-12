3b59fbd9f093975892a85b9f309176ed0ea9aca5
Camila Celente. <camilacelente@yahoo.com.br>
2025-08-12 20:40:02 -0300

    Create PROG001.cob

---
+++ PROG001.cob
@@ -0,0 +1,9 @@
+000001        IDENTIFICATION                  DIVISION.
+000002        PROGRAM-ID. PROG001.                     
+000003        ENVIRONMENT                     DIVISION.
+000004        DATA                            DIVISION.
+000005        WORKING-STORAGE                 SECTION. 
+000006        PROCEDURE                       DIVISION.
+000007           DISPLAY 'PRIMEIRO PROGRAMA'.          
+000008           STOP RUN.                             
+000009                                                 
